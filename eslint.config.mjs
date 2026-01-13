e0fab6ffd114d2685e89593cc6828531da23757f
Vaishnavi Deshmukh <deshmukhvaishnavi2207@gmail.com>
2026-01-13 19:40:45 +0530

    file related to husky and codeQL also ESList added test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import security from "eslint-plugin-security";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,6 +10,42 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    ignores: [
+      ".next/**",
+      "node_modules/**",
+      "public/uploads/**",
+      "build/**",
+      "out/**",
+      "*.log",
+      ".env*"
+    ]
+  },
+  {
+    plugins: {
+      security,
+    },
+    rules: {
+      // Security rules
+      // 'security/detect-non-literal-regexp': 'error',
+      'security/detect-unsafe-regex': 'error',
+      'security/detect-buffer-noassert': 'error',
+      'security/detect-child-process': 'error',
+      'security/detect-disable-mustache-escape': 'error',
+      'security/detect-eval-with-expression': 'error',
+      'security/detect-no-csrf-before-method-override': 'error',
+      // 'security/detect-non-literal-fs-filename': 'error',
+      'security/detect-non-literal-require': 'error',
+      // 'security/detect-possible-timing-attacks': 'error',
+      'security/detect-pseudoRandomBytes': 'error',
+      'no-eval': 'error',
+      'no-implied-eval': 'error',
+      'no-new-func': 'error',
+      'no-script-url': 'error'
+    },
+  },
+];
 
 export default eslintConfig;
